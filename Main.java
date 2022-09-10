1c4873e0e4da291a1d1b09929baeb0ff6ba8289d
nasty8176u <110005925+nasty8176u@users.noreply.github.com>
2022-09-10 16:10:01 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+  public static  void main (String[] args){
+  for (int i = 1; i <= 10; i++) {
+            int triangular = 0;
+
+            for (int j = 1; j <= i; j++) {
+                triangular = triangular + j;
+            }
+            System.out.println(i + " = " + triangular);
+       }
+  }
+}
